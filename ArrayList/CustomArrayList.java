7d8c93fe66fde104cd2e043a62fab86df66dd874
Nikhil <kuntanikhil22@gmail.com>
2025-11-05 14:53:58 +0530

    created separate packages

--- ArrayList/CustomArrayList.java
+++ ArrayList/CustomArrayList.java
@@ -1,3 +1,5 @@
+package ArrayList;
+
 import java.util.Arrays;
 
 public class CustomArrayList {
--- ArrayList/Main.java
+++ ArrayList/Main.java
@@ -1,3 +1,5 @@
+package ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         CustomArrayList list = new CustomArrayList();
