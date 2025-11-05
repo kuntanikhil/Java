3891390e273a6c5d4054527fafc519791b4da0aa
Nikhil <kuntanikhil22@gmail.com>
2025-11-05 13:33:59 +0530

    Succesfully Created my own ArrayList

--- CustomArrayList.java
+++ CustomArrayList.java
@@ -1,5 +1,61 @@
-public class CustomArrayList {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class CustomArrayList {
+
+    final private int DEAFULT_SIZE =10;
+    private int[] data;
+    private int size =0;
+
+    public CustomArrayList(){
+        this.data = new int[DEAFULT_SIZE];
+    }
+
+    public void add(int num){
+        if(isfull()){
+            resize();
+        }
+        data[size++]=num;
+    }
+    public int remove(){
+        int removed=0;
+        if(size<=0) {
+            System.out.println("List is Empty");
+        }else{
+           removed = data[--size];
+        }
+        return removed;
+    }
+
+    public void set(int index , int value){
+        data[index] = value;
+    }
+
+    public int get(int index){
+        return data[index];
+    }
+
+    public void display(){
+        System.out.println(Arrays.toString(data));
+    }
+
+    public int getsize(){
+        return size;
+    }
+
+    public int getcapacity(){
+        return data.length;
+    }
+
+    private void resize() {
+        int size = (data.length)*2;
+        int[] temp =  new int[size];
+        for(int i=0;i< data.length;i++){
+            temp[i]=data[i];
+        }
+        data=temp;
+    }
+
+    private boolean isfull() {
+        return size== data.length;
     }
 }
